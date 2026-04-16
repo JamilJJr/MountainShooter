3d57ed7cd764f87b177abe3d159052a4cb0ab63f
JamilJJr <jamiljjr@gmail.com>
2026-04-15 21:42:01 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+
+print('Janela')
+window = pygame.display.set_mode(size=(600, 480))
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # Encerra a inicialização do PyGame
