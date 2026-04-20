949045bffc725d0883f0bc7c45bfb7a29d56b9e2
JamilJJr <jamiljjr@gmail.com>
2026-04-20 15:05:10 -0300

    Creating main loop - Connecting Meno and Game classes

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import pygame
 
-print('Setup Start')
-pygame.init()
+from code.Game import Game
 
-print('Janela')
-window = pygame.display.set_mode(size=(600, 480))
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('Quitting...')
-            pygame.quit() # Close Window
-            quit() # Encerra a inicialização do PyGame
+game = Game()
+game.run()
