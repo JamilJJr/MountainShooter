5e320ad1726c8f767530c29d24788d47cf228ae5
JamilJJr <jamiljjr@gmail.com>
2026-04-20 16:12:43 -0300

    Menu Ajustes

---
+++ Const.py
@@ -0,0 +1,20 @@
+# Constantes
+# Tem que ser maiúsculas segundo padronização
+
+# C
+
+COLOR_RANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,54 +1,33 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import pygame.image
+import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from Const import WIN_WIDTH, COLOR_RANGE, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
+        self.surf = pygame.image.load('./asset/MenuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self):
-        menu_option = 0
+    def run(self, ):
         pygame.mixer_music.load('./asset/Menu.mp3')
-        pygame.mixer_music.play(-1)
+        pygame.mixer_music.play(-1)  # Toca só uma vez e de por (-1) ela fica tocando sem parar
         while True:
-            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(text_size=50, text='Mountain', text_color=COLOR_RANGE, text_center_pos=((WIN_WIDTH/2), 70))
+            self.menu_text(text_size=50, text='Shooter', text_color=COLOR_RANGE, text_center_pos=((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
-                else:
-                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                self.menu_text(text_size=20, MENU_OPTION[i], text_color=COLOR_WHITE,text_center_pos=((WIN_WIDTH / 2), 180 + 25 * i))
             pygame.display.flip()
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
-                    quit()  # end pygame
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_DOWN:  # DOWN KEY
-                        if menu_option < len(MENU_OPTION) - 1:
-                            menu_option += 1
-                        else:
-                            menu_option = 0
-                    if event.key == pygame.K_UP:  # UP KEY
-                        if menu_option > 0:
-                            menu_option -= 1
-                        else:
-                            menu_option = len(MENU_OPTION) - 1
-                    if event.key == pygame.K_RETURN:  # ENTER
-                        return MENU_OPTION[menu_option]
+                    quit()  # End pygame
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
